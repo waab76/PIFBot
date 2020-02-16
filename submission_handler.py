90e34c8aa51ffdf2da2b681f0fc434bfa3a5c8f0
waab@me.com <ec2-user@ip-172-26-4-29.us-west-2.compute.internal>
2020-02-16 02:07:53 +0000

    Beginnings of PIF handling

--- submission_handler.py
+++ submission_handler.py
@@ -20,11 +20,32 @@
 ############################################################################
 
 def handle_submission(submission):
-    print("Processing post: " + submission.title)
-
     # Decide what kind of post this is and proceed appropriately.  Maybe check
     # the flair to see if it's "PIF - Open" and then kick it over to a PIF
     # handler?
     if submission.link_flair_text == "PIF - Open":
-        print("Found a PIF!")
+        handle_pif(submission)
 
+def handle_pif(submission):
+    lines = submission.selftext.lower().split("\n")
+    for line in lines:
+        if line.startswith("pifbot"):
+            parts = line.split()
+            if parts[1] == "lottery":
+                handle_lottery(submission, parts[2:])
+            elif parts[1] == "range":
+                handle_range(submission, parts[2:])
+            elif parts[1] == "poker":
+                handle_poker(submission, parts[2:])
+
+def handle_lottery(submission, args):
+    print("Lottery PIF")
+    print(args)
+
+def handle_range(submission, args):
+    print("Range PIF -- UNSUPPORTED")
+    print(args)
+
+def handle_poker(submission, args):
+    print("Poker PIF -- UNSUPPORTED") 
+    print(args)                
