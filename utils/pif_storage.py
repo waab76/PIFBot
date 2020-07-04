b5170bd70f9b5f882118d8d5a22166b8ae882627
Rob Curtis <waab@me.com>
2020-07-04 09:50:54 -0400

    Remove in-memory PIF cache

--- utils/pif_storage.py
+++ utils/pif_storage.py
@@ -24,12 +24,12 @@ from utils import dynamo_helper
 from pifs.pif_builder import build_from_ddb_dict
 
 logging.info('Creating PIF cache')
-pif_cache = dict()
+# pif_cache = dict()
 
 def save_pif(pif_obj):
     logging.info('Saving PIF [%s]', pif_obj.postId)
     dynamo_helper.save_pif(pif_obj)
-    pif_cache[pif_obj.postId] = pif_obj
+    # pif_cache[pif_obj.postId] = pif_obj
 
 def get_open_pifs():
     logging.info('Fetching open PIFs')
@@ -38,31 +38,31 @@ def get_open_pifs():
     for json_pif in json_pifs:
         pif_obj = build_from_ddb_dict(json_pif)
         pif_objs.append(pif_obj)
-        if pif_obj.postId not in pif_cache:
-            pif_cache[pif_obj.postId] = pif_obj
+        # if pif_obj.postId not in pif_cache:
+        #     pif_cache[pif_obj.postId] = pif_obj
     return pif_objs
 
 def pif_exists(post_id):
-    if post_id in pif_cache:
-        logging.debug('PIF [%s] found in cache', post_id)
-        return True
-    else:
+    #if post_id in pif_cache:
+    #    logging.debug('PIF [%s] found in cache', post_id)
+    #    return True
+    #else:
         # logging.debug('PIF [%s] not in cache, looking in DDB', post_id)
         # return dynamo_helper.open_pif_exists(post_id)
-        return False
+    return False
 
 def get_pif(post_id):
-    if post_id in pif_cache:
-        logging.info('PIF [%s] found in cache', post_id)
-        return pif_cache[post_id]
+    #if post_id in pif_cache:
+    #    logging.info('PIF [%s] found in cache', post_id)
+    #    return pif_cache[post_id]
+    #else:
+    #    logging.info('PIF [%s] not in cache, looking in DDB', post_id)
+    pif_dict = dynamo_helper.fetch_pif(post_id)
+    if pif_dict is None:
+        logging.warning('PIF [%s] not found in DDB', post_id)
+        return None
     else:
-        logging.info('PIF [%s] not in cache, looking in DDB', post_id)
-        pif_dict = dynamo_helper.fetch_pif(post_id)
-        if pif_dict is None:
-            logging.warning('PIF [%s] not found in DDB', post_id)
-            return None
-        else:
-            pif_obj = build_from_ddb_dict(pif_dict)
-            pif_cache[pif_obj.postId] = pif_obj
-            return pif_obj
+        pif_obj = build_from_ddb_dict(pif_dict)
+        # pif_cache[pif_obj.postId] = pif_obj
+        return pif_obj
     
